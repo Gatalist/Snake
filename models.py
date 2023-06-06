2e97b2250ee612fba9cef5ff028ba704d3191698
Alexander Kostenko <54170213+Gatalist@users.noreply.github.com>
2023-06-06 16:49:55 +0300

    Update and rename snake.py to models.py

--- models.py
+++ models.py
@@ -162,26 +162,3 @@ class Food(Game):
 
 	def draw_food(self):
 		self.render_block(self.food_pos[0], self.food_pos[1], self.food_color)
-
-
-game = Game()
-snake = Snake()
-food = Food()
-
-
-while True:
-	snake.change_to = snake.event_key()
-	snake.validate_direction_and_change()
-	snake.do_going()
-
-	game.screen_fill() # закраска экрана
-	game.render_table() # рисуем table
-	
-	snake.draw_snake() # рисуем змейку
-	snake.track_for_tail()
-
-	food.food_eat(snake.snake_block, food.food_pos, game.score)
-	game.show_score()
-	
-	food.draw_food() # рисуем еду
-	game.game_update()
